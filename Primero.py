787561818cca9a2f54d1e0165d2172bc85d8a4f2
JeanethLC <84730787+JeanethLC@users.noreply.github.com>
2021-05-25 01:28:04 +0200

    Add files via upload

---
+++ Primero.py
@@ -0,0 +1,11 @@
+print ('Holaaaa')
+range = [1,3,5,7,9]
+for i in range:
+    print(i)
+
+numero = 0
+suma = 0
+while numero <= 7:
+    suma = suma + numero
+    numero = numero + 1
+print ('la suma total es ' + str(suma))
