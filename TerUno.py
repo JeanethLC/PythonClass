5496dc03bb6826fb9e603c4ba2625ba5fb5b7266
JeanethLC <84730787+JeanethLC@users.noreply.github.com>
2021-05-26 18:58:07 +0200

    Add files via upload

---
+++ TerUno.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestResult(unittest.TestCase):
+    def test_result(self):
+        numero = 0
+        suma = 0
+        while numero <= 7:
+            suma = suma + numero
+            numero = numero + 1
+        print ('la suma total es ' + str(suma))
+        self.assertEqual(suma)
